09c1e980fcc27eb92a1695e574c1c4a8b8b64372
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2021-05-22 20:53:58 +0300

    solving CC05

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,80 @@
-'use strict';
+const Node = require('./linked-list');
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value){
+    this.head.value = new Node(value);
+  }
+
+  include (value){
+    try {
+
+      let valueCheck = false;
+      let nodeCheck = this.head;
+      while(nodeCheck){
+        if (nodeCheck.value === value){
+          valueCheck = true;
+          return valueCheck;
+        }
+        nodeCheck = nodeCheck.next;
+      }
+      return valueCheck;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  toString() {
+    try {
+      let head = this.head;
+      let result = [];
+      while(head) {
+        result.push(' { ');
+        result.push(head.value);
+        result.push(' } ');
+        result.push(' -> ');
+        head = head.next;
+      }
+      result.push('NULL');
+      return result.join('');
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  add(value) {
+    try {
+
+      let node = new Node(value);
+      if (!this.head) {
+        this.head = node;
+      } else {
+        let lastNode = this.head;
+        while (lastNode.next) {
+          lastNode = lastNode.next;
+        }
+        lastNode.next = node;
+      }
+    } catch (error) {
+      console.error(error);
+
+    }
+  }
+
+  // addNewHead(value) {
+  //   let newNode = new Node(value);
+  //   if (!this.head) {
+  //     this.head = newNode;
+  //   } else {
+  //     newNode.next = this.head;
+  //     this.head = newNode;
+  //   }
+  // }
 }
 
 module.exports = LinkedList;
